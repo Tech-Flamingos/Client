96388b17079dc4cd385175f203943d4e7fe65dd4
MARTIN HANSEN <marrtt1234@gmail.com>
2023-03-18 10:42:00 +0000

    each user can make new rooms and join users rooms

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 'use strict';
 const { io } = require('socket.io-client');
-// const socket = io.connect('http://localhost:3001/games');
-const socket = io.connect('https://adventureai-server.onrender.com/games');
+const socket = io.connect('http://localhost:3001/games');
+// const socket = io.connect('https://adventureai-server.onrender.com/games');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 
-socket.emit('JOIN', 'adventureGame');
-
-const login = () => {
+function login(){
   inquirer.prompt([{ type: 'list', name: 'sign', message: chalk.green('Welcome to Adventure AI!'), choices: ['Sign in', 'Sign up', 'Continue as guest'] }]).then((answers) => {
+    let userObj = {};
     if (answers.sign === 'Sign up') {
-      let userObj = {};
       inquirer.prompt([{ type: 'input', name: 'name', message: 'Choose a username' }]).then((answers) => {
         userObj = { ...userObj, ...answers };
         inquirer.prompt([{ type: 'input', name: 'password', message: 'Create a password' }]).then((answers) => {
@@ -22,7 +20,6 @@ const login = () => {
         });
       });
     } else if (answers.sign === 'Sign in') {
-      let userObj = {};
       inquirer.prompt([{ type: 'input', name: 'name', message: 'Enter Username' }]).then((answers) => {
         userObj = { ...userObj, ...answers };
         inquirer.prompt([{ type: 'input', name: 'password', message: 'Enter Password' }]).then((answers) => {
@@ -31,7 +28,7 @@ const login = () => {
         });
       });
     } else {
-      console.log('continue as guest');
+      console.log('app no work sorry');
     }
   }).catch((error) => {
     if (error.isTtyError) {
@@ -40,9 +37,23 @@ const login = () => {
       console.log('Something else went wrong');
     }
   });
-};
+}
 
-const main = async (user) => {
+function roomBool(payload){
+  inquirer.prompt([{ type: 'list', name: 'roomBool', message: chalk.green('Adventure AI'), choices: ['New Game', 'View active games'] }]).then((answers) => {
+    if (answers.roomBool === 'New Game') {
+      socket.emit('NEW-GAME', payload);
+    }
+    // else if (answers.roomBool === 'Continue game') {
+    //   socket.emit('CONTINUE-GAME', {...payload, ...answers});
+    // } 
+    else {
+      socket.emit('VIEW-GAMES', payload);
+    }
+  });
+}
+
+async function main(user){
 
   console.log(chalk.green(`Hello ${user.user.name}`));
   let userWantsToExit = false;
@@ -56,16 +67,29 @@ const main = async (user) => {
     });
   }
   socket.disconnect();
-};
+}
 
 socket.on('AI-REPLY', payload => {
   console.log('AI: ', payload.message);
 });
 
 socket.on('LOGGED-IN', payload => {
-  main(payload);
-  payload.message = 'hello';
-  socket.emit('NEW-MESSAGE', payload);
+  roomBool(payload);
+  // main(payload);
+  // payload.message = 'hello';
+  // socket.emit('NEW-MESSAGE', payload);
+});
+
+socket.on('ALL-ROOMS', payload => {
+  let q = payload.allRooms;
+  if (payload.allRooms.length) {
+    inquirer.prompt([{ type: 'list', name: 'userRoom', message: chalk.green('Available rooms'), choices: q }]).then((answers) => {
+      socket.emit('JOIN-ROOM', {...answers, ...payload});
+    });
+  } else {
+    console.log('No available rooms');
+    roomBool(payload);
+  }
 });
 
 login();
